f90e5963b1eb631d9b35c617e16c70da8da50630
CHEN <cjpnice@qq.com>
2022-04-16 11:39:18 +0800

    第10题

---
+++ 10.正则表达式匹配.py
@@ -0,0 +1,46 @@
+#
+# @lc app=leetcode.cn id=10 lang=python
+#
+# [10] 正则表达式匹配
+#
+
+# @lc code=start
+
+
+
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        m, n = len(s), len(p)
+        def matches(i, j):
+            if i == 0:
+                return False
+            if p[j - 1] == '.':
+                return True
+            return s[i - 1] == p[j - 1]
+        
+
+        f = [[False] * (n + 1) for _ in range(m + 1)]
+        f[0][0] = True
+        for i in range(m + 1):
+            for j in range(1, n + 1):
+                if p[j - 1] == '*':
+                    f[i][j] |= f[i][j - 2]
+                    if matches(i, j - 1):
+                        f[i][j] |= f[i - 1][j]
+                else:
+                    if matches(i, j):
+                        f[i][j] |= f[i - 1][j - 1]
+        return f[m][n]
+
+
+            
+
+# @lc code=end
+if __name__ == "__main__":
+  solution = Solution()
+  solution.isMatch("aa", "a*")
